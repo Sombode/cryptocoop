a2440828c57e43f0b699ebdbe4665d5e9b083cf0
Sombode <25levyle@gmail.com>
2024-02-24 01:00:51 -0500

    chore: forgot to fully purge opponentprogress

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@ export { default as CryptogramSolver } from './CryptogramSolver.svelte';
 export { default as NameChooser } from './NameChooser.svelte';
 export { default as Word } from './Word.svelte';
 export { default as JoinLink } from './JoinLink.svelte';
-export { default as OpponentProgress } from './OpponentProgress.svelte';
 export { default as Lobby } from './Lobby.svelte';
 export { default as ReplacementTable } from './ReplacementTable.svelte';
 export { default as FirstLaunchPrompt } from './FirstLaunchPrompt.svelte';
