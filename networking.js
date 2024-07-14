988feeeeec460cd9c57e9144803837b369a54b42
Sombode <25levyle@gmail.com>
2024-07-14 02:48:14 -0400

    Feature: Max room size of 3

--- networking.js
+++ networking.js
@@ -1,99 +1,63 @@
 const peer = new Peer();
 let hiveBrain = new URLSearchParams(location.search).get('room');
 let isHiveBrain = hiveBrain === null;
+// REFACTOR: might be able to remove this
 let hiveBrainConn;
 let id;
+// consider changing the map to an array?
 const connections = new Map();
 const Messages = {
     INIT: 0,
-    NEW_QUOTE: 1,
-    REPLACE_LETTER: 2,
+    ERROR: 1,
+    NEW_QUOTE: 2,
+    REPLACE_LETTER: 3,
 }
 
-const arraySubscription = (target, updateFunc) => new Proxy(target, {
-  set(target, prop) {
-    // array.push() hits this trap twice as
-    // it also sets the length (which counts I guess)
-    
-    // also, using setTimeout here to delay updateFunc to the next event cycle
-    // since the array doesn't actually get modified until afterwards
-    if(prop != "length") setTimeout(updateFunc);
-    return Reflect.set(...arguments);
-  }
-});
+let players = new Array();
 
-//let players = [];
-let players = arraySubscription([], () => {
-  console.log(`Updated Players ${players}`);
-})
+const MAX_PLAYERS = 2;
 
-// const connections = subscription(new Map(), () => {
-//   console.log(connections);
-// });
+const emit = (msg) => connections.forEach((conn) => conn.send(msg));
 
-// class SubscribableArray extends Array {
-//   constructor(updateFunc) {
-//     super();
-//     this.updateFunc = updateFunc;
-//   }
-//   set(...args) {
-//     console.log("SET");
-//     this.updateFunc();
-//     return super.set(...args);
-//   }
-// }
-
-// let players = new SubscribableArray(() => {
-//   console.log("PLAYERS UPDATED");
-// });
-
-const emit = (msg) =>
-  connections.forEach((conn) => conn.send(msg));
-
-const onData = (targetId) => (data) => {
-  console.log(`Recieved data! ${data}`);
-  if (messageHandlers[data.type]) messageHandlers[data.type](targetId, data);
-};
+const onData = (targetId) => (data) => (messageHandlers[data.type] ? messageHandlers[data.type](targetId, data) : null);
 
 const removePlayer = (targetId) => () => {
-    players = players.filter((player) => player.id !== targetId);
-    connections.delete(targetId);
-  };
+  players = players.filter((player) => player.id !== targetId);
+  connections.delete(targetId);
+};
 
 const sendInitialState = (conn) =>
-    (conn === null || conn === void 0 ? void 0 : conn.send({
-      type: Messages.INIT,
-      // TODO: Add names!
-      name: id,
-    }));
+  (conn ? conn.send({
+    type: Messages.INIT,
+    // TODO: Add names!
+    name: id,
+  }) : null);
 
 const openConnection = (conn) =>
-    new Promise((res) => {
-      conn.on('open', () => {
-        if (conn.peer === hiveBrain) hiveBrainConn = conn;
-        connections.set(conn.peer, conn);
-        conn.on('data', onData(conn.peer));
-        // TODO: handle disconnects by polling
-        // dataChannel.peerConnection.iceConnectionState
-        conn.on('close', removePlayer(conn.peer));
-        setTimeout(() => {
-          sendInitialState(conn);
-        }, 100);
-        res(conn);
-      });
+  new Promise((res) => {
+    conn.on('open', () => {
+      if (conn.peer === hiveBrain) hiveBrainConn = conn;
+      connections.set(conn.peer, conn);
+      conn.on('data', onData(conn.peer));
+      // TODO: handle disconnects by polling
+      // dataChannel.peerConnection.iceConnectionState
+      conn.on('close', removePlayer(conn.peer));
+      setTimeout(() => { sendInitialState(conn) }, 100);
+      res(conn);
     });
+  });
 
 const connectTo = (targetId) =>
-    new Promise((res) => {
-      const conn = peer.connect(targetId);
-      openConnection(conn).then(res);
-      // const unsub = id.subscribe(($id) => {
-      //   if ($id === '') return;
-      //   const conn = peer.connect(targetId);
-      //   setTimeout(() => unsub());
-      //   openConnection(conn).then(res);
-      // });
-    });
+  new Promise((res) => {
+    const conn = peer.connect(targetId);
+    openConnection(conn).then(res);
+    // const unsub = id.subscribe(($id) => {
+    //   if ($id === '') return;
+    //   const conn = peer.connect(targetId);
+    //   setTimeout(() => unsub());
+    //   openConnection(conn).then(res);
+    // });
+  });
 
 const initializePlayer = (id, data) => {
   players.push({
@@ -104,32 +68,42 @@ const initializePlayer = (id, data) => {
   });
 };
 
-const onNewQuote = (id, data) => {
-  if(!isHiveBrain) insertQuote(data.quote);
-}
+const handleError = (_, data) => console.error(data.error);
 
-const remoteReplaceLetter = (id, data) => {
-  replaceLetter(data.letter, data.replacement);
-}
+const onNewQuote = (_, data) => insertQuote(data.quote);
+
+const remoteReplaceLetter = (_, data) => replaceLetter(data.letter, data.replacement);
 
 const messageHandlers = [
   initializePlayer,
+  handleError,
   onNewQuote,
   remoteReplaceLetter
 ]
+
+function handleConnection(conn) {
+  if(isHiveBrain && players.length >= MAX_PLAYERS) {
+    // If the room is full, let the incoming connection know before closing it
+    conn.on("open", () => {
+      conn.send({
+        type: Messages.ERROR,
+        // Consider error codes if there are enough types of errors
+        error: "The room is full."
+      });
+      conn.close();
+    });
+    return;
+  }
+  openConnection(conn);
+}
 
 peer.on("open", ($id) => {
     id = $id;
     document.getElementById("debugId").innerText = `${(isHiveBrain ? "[HIVEBRAIN] " : "")} ${id}`;
     if(!isHiveBrain && hiveBrain != null) connectTo(hiveBrain);
-    console.info(isHiveBrain
-    ? `${window.location.href}?room=${encodeURIComponent(id)}`
-    : window.location.href);
-
+    console.info(isHiveBrain ? `${window.location.href}?room=${encodeURIComponent(id)}` : window.location.href);
 });
 
-peer.on('connection', openConnection);
+peer.on('connection', handleConnection);
 
-peer.on('error', (e) => {
-  console.error(e);
-});
+peer.on('error', (e) => console.error(e));
